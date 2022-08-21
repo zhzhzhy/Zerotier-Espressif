1650262aefa66b0521a54b97d97e499713ff83e5
geek <by6dx1@gmail.com>
2022-08-21 16:39:39 +0800

    change lwip to esp-lwip

---
+++ examples/libzt/build/bootloader-prefix/tmp/bootloader-mkdirs.cmake
@@ -0,0 +1,19 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "/home/zzy/esp/esp-idf/components/bootloader/subproject"
+  "/home/zzy/PROJ/Zerotier-Espressif/examples/libzt/build/bootloader"
+  "/home/zzy/PROJ/Zerotier-Espressif/examples/libzt/build/bootloader-prefix"
+  "/home/zzy/PROJ/Zerotier-Espressif/examples/libzt/build/bootloader-prefix/tmp"
+  "/home/zzy/PROJ/Zerotier-Espressif/examples/libzt/build/bootloader-prefix/src/bootloader-stamp"
+  "/home/zzy/PROJ/Zerotier-Espressif/examples/libzt/build/bootloader-prefix/src"
+  "/home/zzy/PROJ/Zerotier-Espressif/examples/libzt/build/bootloader-prefix/src/bootloader-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "/home/zzy/PROJ/Zerotier-Espressif/examples/libzt/build/bootloader-prefix/src/bootloader-stamp/${subDir}")
+endforeach()
