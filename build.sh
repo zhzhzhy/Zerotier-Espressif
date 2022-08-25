e7d4a394aae3a70dda47b310693f31a845e675ae
geek <by6dx1@gmail.com>
2022-08-25 10:17:01 +0800

    fix undefined refer

--- build.sh
+++ build.sh
@@ -141,7 +141,7 @@ TARGET=$(echo $0 | cut -s -f2 -d- | cut -s -f1 -d.)
 if [[ -n $TARGET ]]
 then
     # Target is not null, specify the build parameters
-    PARAM="-DCMAKE_TOOLCHAIN_FILE=$IDF_PATH/tools/cmake/toolchain-${TARGET}.cmake -DTARGET=${TARGET}"
+    PARAM="-DCMAKE_TOOLCHAIN_FILE=$IDF_PATH/tools/cmake/toolchain-${TARGET}.cmake -DTARGET=${TARGET} "
 fi
 
 rm -rf build && mkdir build && cd build
