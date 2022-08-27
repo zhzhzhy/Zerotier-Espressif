f5e6922106d3fe51c7d45212fda1a66c308d5449
geek <by6dx1@gmail.com>
2022-08-27 23:53:56 +0800

    include_directories({IDF_PATH}/components/lwip/port/esp32/include)

---
+++ build/libzt/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/zzy/PROJ/Zerotier-Espressif/libzt
+# Build directory: /home/zzy/PROJ/Zerotier-Espressif/build/libzt
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(selftest-c "/home/zzy/PROJ/Zerotier-Espressif/build/libzt/bin/selftest-c")
+set_tests_properties(selftest-c PROPERTIES  _BACKTRACE_TRIPLES "/home/zzy/PROJ/Zerotier-Espressif/libzt/CMakeLists.txt;693;add_test;/home/zzy/PROJ/Zerotier-Espressif/libzt/CMakeLists.txt;0;")
