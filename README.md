2c09d8e893f1d1fbfe96a350f3acf557449f1d25
geek <by6dx1@gmail.com>
2022-08-29 22:45:48 +0800

    error picture

--- README.md
+++ README.md
@@ -18,3 +18,9 @@ OR
 
 use [VScode extension](https://github.com/espressif/vscode-esp-idf-extension/blob/master/docs/tutorial/install.md) 
 After installation click **ESP-IDF Build project** Button
+
+## Known Errors
+
+After ESP-IDF component build complete,libzt is the last component being built.
+The compiler says 'Mutex' does not name a type ‘(Maybe The thread lock in FreeRTOS is't compatible with libzt itself)
+![Error1](img/error1.png)
---
+++ build/esp-idf/components/libzt_esp/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/zzy/PROJ/Zerotier-Espressif/components/libzt_esp
+# Build directory: /home/zzy/PROJ/Zerotier-Espressif/build/esp-idf/components/libzt_esp
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(selftest-c "/home/zzy/PROJ/Zerotier-Espressif/build/esp-idf/components/libzt_esp/bin/selftest-c")
+set_tests_properties(selftest-c PROPERTIES  _BACKTRACE_TRIPLES "/home/zzy/PROJ/Zerotier-Espressif/components/libzt_esp/CMakeLists.txt;638;add_test;/home/zzy/PROJ/Zerotier-Espressif/components/libzt_esp/CMakeLists.txt;0;")
