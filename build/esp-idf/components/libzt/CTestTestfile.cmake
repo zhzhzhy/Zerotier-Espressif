827b32cc32c0b19a252389194fd1f1a45a4e48fe
geek <by6dx1@gmail.com>
2022-08-29 22:37:19 +0800

    fix "add_subdirectory given source "libzt" which is not an existing directory."

--- build/esp-idf/components/libzt/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zzy/PROJ/Zerotier-Espressif/components/libzt
-# Build directory: /home/zzy/PROJ/Zerotier-Espressif/build/esp-idf/components/libzt
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(selftest-c "/home/zzy/PROJ/Zerotier-Espressif/build/esp-idf/components/libzt/bin/selftest-c")
-set_tests_properties(selftest-c PROPERTIES  _BACKTRACE_TRIPLES "/home/zzy/PROJ/Zerotier-Espressif/components/libzt/CMakeLists.txt;638;add_test;/home/zzy/PROJ/Zerotier-Espressif/components/libzt/CMakeLists.txt;0;")
